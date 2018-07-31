214182fb6b09843dcea62de54e7f4368ebd65063
Siva Dasu <sivadasu88@gmail.com>
2018-08-01 00:38:42 +0530

    Pushing the changes to master branch

--- SD.Microservice2/Program.cs
+++ SD.Microservice2/Program.cs
@@ -16,7 +16,6 @@ namespace SD.Microservice2
         {
             BuildWebHost(args).Run();
         }
-
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
